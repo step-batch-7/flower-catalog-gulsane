5e135c50e8bd52cbaef62215c10336c06e8e92e7
gulshan kumar <gulhan@123>
2020-01-29 19:33:07 +0530

    Refactored code inside module app.js
    Separated the postCheck from serveGuestPage function
    It will responsible for only the GET method
    
    Returning the reference of postComment in postHandler for
    for request url </guestPage.html>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const { createTable, readParams } = require("./lib/utils.js");
-const CONTENT_TYPES = require("./lib/mediaType.js");
+const fs = require('fs');
+const { createTable, readParams } = require('./lib/utils.js');
+const CONTENT_TYPES = require('./lib/mediaType.js');
 
 const getStaticFolder = () => {
   const STATIC_FOLDER = `${__dirname}/public`;
@@ -11,43 +11,62 @@ const savePost = function(body, comments) {
   const comment = readParams(body);
   comment.date = new Date().toJSON();
   comments.unshift(comment);
-  fs.writeFileSync("./userComments.json", `${JSON.stringify(comments)}`);
+  fs.writeFileSync('./userComments.json', `${JSON.stringify(comments)}`);
+};
+
+const postComment = function(req, res) {
+  let comments = fs.readFileSync('./userComments.json', 'utf8');
+  comments = JSON.parse(comments);
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk;
+  });
+  req.on('end', () => {
+    savePost(body, comments);
+  });
+  const statusCode = 303;
+  res.writeHead(statusCode, { Location: '/guestBook.html' });
+  res.end();
 };
 
 const serveGuestBook = function(req, res) {
-  let comments = fs.readFileSync("./userComments.json", "utf8");
+  let comments = fs.readFileSync('./userComments.json', 'utf8');
   comments = JSON.parse(comments);
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", chunk => (body += chunk));
-    req.on("end", () => {
-      savePost(body, comments);
-    });
-    res.writeHead(303, { Location: `/guestBook.html` });
-    res.end();
-  } else {
-    const commentTable = createTable(comments);
-    let content = fs.readFileSync("./public/guestBook.html", "utf8");
-    content = content.replace("__comments__", commentTable);
-    res.write(content);
-    res.end();
-  }
-};
-
-const serveStaticFile = (req, res) => {
-  const [, extension] = req.url.match(/.*\.(.*)$/) || [];
-  const path = `${getStaticFolder()}${req.url}`;
-  const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) {
-    res.writeHead(404, "Not Found");
-    res.end();
-    return;
-  }
-  const contentType = CONTENT_TYPES[extension];
-  const content = fs.readFileSync(path);
-  res.writeHead(200, { "content-type": contentType });
+  const commentTable = createTable(comments);
+  let content = fs.readFileSync('./public/guestBook.html', 'utf8');
+  content = content.replace('__comments__', commentTable);
   res.write(content);
   res.end();
 };
 
-module.exports = { serveGuestBook, serveStaticFile };
+const respondError = function(res) {
+  const statusCode = 404;
+  res.writeHead(statusCode, 'Not Found');
+  res.end();
+};
+
+const getExtension = url => {
+  const [, extension] = url.match(/.*\.(.*)$/) || [];
+  return extension;
+};
+
+const respondContent = function(req, res, content) {
+  const extension = getExtension(req.url);
+  const statusCode = 200;
+  res.writeHead(statusCode, { 'content-type': CONTENT_TYPES[extension] });
+  res.write(content);
+  res.end();
+};
+
+const serveStaticFile = function(req, res) {
+  const path = `${getStaticFolder()}${req.url}`;
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) {
+    respondError(res);
+    return;
+  }
+  const content = fs.readFileSync(path);
+  respondContent(req, res, content);
+};
+
+module.exports = { serveGuestBook, serveStaticFile, postComment };
--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const { Server } = require("http");
-const { methods } = require("./lib/handlers.js");
+const { Server } = require('http');
+const { methods } = require('./lib/handlers.js');
+const port = process.argv[2] || 4000;
 
 const handleConnection = function(req, res) {
-  console.log(req.url, req.method);
   const handlers = methods[req.method] || methods.NOT_ALLOWED;
   const handler = handlers[req.url] || handlers.defaultHandler;
   return handler(req, res);
 };
 
-const main = (port = 4000) => {
+const main = port => {
   const server = new Server(handleConnection);
-  server.on("listening", () =>
-    console.warn("server started listening", server.address())
-  );
   server.listen(port);
 };
 
-main(process.argv[2]);
+main(port);
