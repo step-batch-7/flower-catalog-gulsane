91a13937b930709ab5007549686a257531f56f94
gulshan kumar <gulhan@123>
2020-01-30 10:39:50 +0530

    Added App class
    Added matchHandlers function to the class App
    It provides flexibility to choose the handlers
    according to the request
    
    Added next concept to the class App
    It perform the response task sequencially in predefined path
    
    Commit Headline/ Feature
    
    What changes you have made?
    .....
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const { Server } = require('http');
-const { methods } = require('./lib/handlers.js');
+const { app } = require('./lib/handlers.js');
 const port = process.argv[2] || 4000;
 
-const handleConnection = function(req, res) {
-  const handlers = methods[req.method] || methods.NOT_ALLOWED;
-  const handler = handlers[req.url] || handlers.defaultHandler;
-  return handler(req, res);
-};
-
 const main = port => {
-  const server = new Server(handleConnection);
+  const server = new Server(app.serve.bind(app));
   server.listen(port);
 };
 
